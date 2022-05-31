ce3fd717a4453abaef9b143aeec15a82639b3b2f
Erik Huckvale <42946548+erikhuck@users.noreply.github.com>
2022-05-31 17:11:49 -0400

    removes ssl redirect setting

--- back_end/settings/staging.py
+++ back_end/settings/staging.py
@@ -2,4 +2,4 @@ from back_end.settings.hosting_base import *
 
 CORS_ALLOWED_ORIGINS = ['https://sbcoa-app-cdna-comparison-staging.azurewebsites.com']  # Frontend URL
 # ALLOWED_HOSTS = ['https://sbcoa-api-cdna-comparison-staging.azurewebsites.com']  # Backend URL
-ALLOWED_HOSTS = ['cdna-comparison.azurewebsites.net']  # Backend URL
+ALLOWED_HOSTS = ['cdna-comparison.azurewebsites.net', 'localhost']  # Backend URL
